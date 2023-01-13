1b103689327ad8beca8b450699143329796b615c
Sean Connole <seanconnole32@gmail.com>
2023-01-12 22:06:33 -0700

    fix: removed default formatting setting

--- nvim/lua/lsp/ts.lua
+++ nvim/lua/lsp/ts.lua
@@ -6,43 +6,43 @@ local buf_map = function(bufnr, mode, lhs, rhs, opts)
 	})
 end
 
-	local on_attach = function(client, bufnr)
-		buf_map(bufnr, "n", "gs", ":TSLspOrganize<CR>")
-		buf_map(bufnr, "n", "gi", ":TSLspRenameFile<CR>")
-		buf_map(bufnr, "n", "go", ":TSLspImportAll<CR>")
-		vim.cmd("command! LspDef lua vim.lsp.buf.definition()")
-		vim.cmd("command! LspCodeAction lua vim.lsp.buf.code_action()")
-		vim.cmd("command! LspRangeCodeAction lua vim.lsp.buf.range_code_action()")
-		vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
-		vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
-		vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
-		vim.cmd("command! LspTypeDef lua vim.lsp.buf.type_definition()")
-		vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
-		vim.cmd("command! LspDiagPrev lua vim.diagnostic.goto_prev()")
-		vim.cmd("command! LspDiagNext lua vim.diagnostic.goto_next()")
-		vim.cmd("command! LspDiagLine lua vim.diagnostic.open_float()")
-		vim.cmd("command! LspSignatureHelp lua vim.lsp.buf.signature_help()")
-		buf_map(bufnr, "n", "gd", ":LspDef<CR>")
-		buf_map(bufnr, "n", "gr", ":LspRename<CR>")
-		buf_map(bufnr, "n", "gy", ":LspTypeDef<CR>")
-		buf_map(bufnr, "n", "K", ":LspHover<CR>")
-		buf_map(bufnr, "n", "[a", ":LspDiagPrev<CR>")
-		buf_map(bufnr, "n", "]a", ":LspDiagNext<CR>")
-		buf_map(bufnr, "n", "<leader>ca", ":LspCodeAction<CR>")
-		buf_map(bufnr, "n", "<leader>cra", ":LspRangeCodeAction<CR>")
-		buf_map(bufnr, "n", "<Leader>a", ":LspDiagLine<CR>")
-		buf_map(bufnr, "i", "<C-x><C-x>", "<cmd> LspSignatureHelp<CR>")
-		vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format({ async = false })")
-		-- vim.cmd("command! LspFormatting lua vim.lsp.buf.format({ async = true })")
+local on_attach = function(client, bufnr)
+	buf_map(bufnr, "n", "gs", ":TSLspOrganize<CR>")
+	buf_map(bufnr, "n", "gi", ":TSLspRenameFile<CR>")
+	buf_map(bufnr, "n", "go", ":TSLspImportAll<CR>")
+	vim.cmd("command! LspDef lua vim.lsp.buf.definition()")
+	vim.cmd("command! LspCodeAction lua vim.lsp.buf.code_action()")
+	vim.cmd("command! LspRangeCodeAction lua vim.lsp.buf.range_code_action()")
+	vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
+	vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
+	vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
+	vim.cmd("command! LspTypeDef lua vim.lsp.buf.type_definition()")
+	vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
+	vim.cmd("command! LspDiagPrev lua vim.diagnostic.goto_prev()")
+	vim.cmd("command! LspDiagNext lua vim.diagnostic.goto_next()")
+	vim.cmd("command! LspDiagLine lua vim.diagnostic.open_float()")
+	vim.cmd("command! LspSignatureHelp lua vim.lsp.buf.signature_help()")
+	buf_map(bufnr, "n", "gd", ":LspDef<CR>")
+	buf_map(bufnr, "n", "gr", ":LspRename<CR>")
+	buf_map(bufnr, "n", "gy", ":LspTypeDef<CR>")
+	buf_map(bufnr, "n", "K", ":LspHover<CR>")
+	buf_map(bufnr, "n", "[a", ":LspDiagPrev<CR>")
+	buf_map(bufnr, "n", "]a", ":LspDiagNext<CR>")
+	buf_map(bufnr, "n", "<leader>ca", ":LspCodeAction<CR>")
+	buf_map(bufnr, "n", "<leader>cra", ":LspRangeCodeAction<CR>")
+	buf_map(bufnr, "n", "<Leader>a", ":LspDiagLine<CR>")
+	buf_map(bufnr, "i", "<C-x><C-x>", "<cmd> LspSignatureHelp<CR>")
+	-- vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format({ async = false })")
+	-- vim.cmd("command! LspFormatting lua vim.lsp.buf.format({ async = true })")
 end
 
 require("typescript").setup({
-    disable_commands = false, -- prevent the plugin from creating Vim commands
-    debug = false, -- enable debug logging for commands
-    go_to_source_definition = {
-        fallback = true, -- fall back to standard LSP definition on failure
-    },
-    server = { -- pass options to lspconfig's setup method
-        on_attach = on_attach,
-    },
+	disable_commands = false, -- prevent the plugin from creating Vim commands
+	debug = false, -- enable debug logging for commands
+	go_to_source_definition = {
+		fallback = true, -- fall back to standard LSP definition on failure
+	},
+	server = { -- pass options to lspconfig's setup method
+		on_attach = on_attach,
+	},
 })
